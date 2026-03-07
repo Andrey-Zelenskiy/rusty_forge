cbf5cd6b90767c4eed91617aa62e70989c6bf883
Andrey <andrey.zelenskiy@universite-paris-saclay.fr>
2026-03-07 20:36:12 +0100

    Added a module for standardized scalar and array initialization

--- src/lib.rs
+++ src/lib.rs
@@ -8,3 +8,5 @@ pub use crate::builder::{BuildError, BuilderMethods, TargetFromBuilder};
 //pub mod output;
 pub mod project_setup;
 pub use crate::project_setup::ProjectManager;
+
+pub mod initialize;
